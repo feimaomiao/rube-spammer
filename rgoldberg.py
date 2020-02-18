df0250b9b80a6b31b46c8544bbc59277e287ab89
feimaomiao <lcpmatthew@gmail.com>
2020-02-17 21:36:43 -0800

    added say function

--- rgoldberg.py
+++ rgoldberg.py
@@ -1,4 +1,9 @@
-import smtplib, ssl
+import smtplib, ssl, os, time
+"""
+http://bit.ly/thrashergfr
+Created by Matthew Lam
+Used to make my phone vibrate in the rube-goldberg machine
+"""
 port = 465
 smtp_server = "smtp.gmail.com"
 sender_email = "lcpmatthew@gmail.com"
@@ -13,13 +18,16 @@ This is a spamming email sent by myself with python3
 '''
 
 if __name__ == '__main__':
+	os.system('say sending emails')
+	time.sleep(2)
 	context= ssl.create_default_context()
 	with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
 		server.login(sender_email, password)
-		# infinite loop
 		i = 0
-		while True:
-			i += 1
-			server.sendmail(sender_email, receiver, message)
-			print(f'{i} Email(s) sent')
-
+		try:
+			while True:
+				i += 1
+				server.sendmail(sender_email, receiver, message)
+				print(f'{i} Email(s) sent')
+		except KeyboardInterrupt:
+			quit()
